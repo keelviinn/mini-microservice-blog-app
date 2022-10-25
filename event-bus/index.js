8f54d8e33006a27026bb9df7017aaa9b1ff594a6
Kelvin Oliveira <keelviinn@gmail.com>
2022-10-25 23:12:27 +0100

    event bus service

---
+++ event-bus/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios.post('http://localhos:4000/events', event)
+    .catch((err) => console.log(err.message));
+  
+  axios.post('http://localhos:4001/events', event)
+    .catch((err) => console.log(err.message));
+  
+  axios.post('http://localhos:4002/events', event)
+    .catch((err) => console.log(err.message));
+  
+  // axios.post('http://localhos:4003/events', event)
+  //   .catch((err) => console.log(err.message));
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
