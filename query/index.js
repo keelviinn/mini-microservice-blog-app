50ec98e00e4f892554d08c88bd570a4fb94f79cb
Kelvin Oliveira <keelviinn@gmail.com>
2022-10-27 13:30:05 +0100

    moderation service

--- query/index.js
+++ query/index.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get('/posts', (req, res) => {
-  res.send('Hello');
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
@@ -21,10 +23,20 @@ app.post('/events', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
   }
 
   res.send({});
